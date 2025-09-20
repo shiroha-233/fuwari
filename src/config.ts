acbf75475c95d5edc0a53473d3c7bdf9582282fb
emt-1t <2596998506@qq.com>
2025-09-21 07:50:00 +0800

    null

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
-			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
+			name: "bilibili",
+			url: "https://space.bilibili.com/404943467", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
 		},
 	],
